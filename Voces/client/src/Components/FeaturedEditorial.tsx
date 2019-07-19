b705fa96ef9450ac7d3aed3c25b62f8d8fee4a36
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-19 16:24:39 -0700

    add featured Editorial component

---
+++ Voces/client/src/Components/FeaturedEditorial.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../CSS/Featured.css';
+
+export default function FeatureEditorial() {
+  return (
+    <div className="border">
+      {/* <h1>hello</h1> */}
+    </div>
+  )
+}
