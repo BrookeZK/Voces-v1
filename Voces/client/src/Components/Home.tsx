dd3cd880acb32d948b7ae3307e26b9ac85ccef52
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-19 16:26:09 -0700

    add headerHomePage component to homepage as well as container

--- Voces/client/src/Components/Home.tsx
+++ Voces/client/src/Components/Home.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Nav from './Nav';
 import HeaderHomePage from './HeaderHomePage';
+import '../CSS/Home.css';
+import FeaturedEditorial from './FeaturedEditorial';
 
 export default function Home() {
   return (
     <div>
       <HeaderHomePage />
       <Nav />
+      <div className="container">
+        <FeaturedEditorial />
+        <FeaturedEditorial />
+      </div>
     </div>
   )
 }
