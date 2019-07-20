358d54164af7937c05a9913a49eae30398da6b63
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-19 17:16:22 -0700

    add featured news component

---
+++ Voces/client/src/Components/FeaturedNews.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../CSS/Featured.css';
+
+export default function FeatureNews() {
+  return (
+    <div className="border">
+      <h1>Featured News</h1>
+    </div>
+  )
+}
