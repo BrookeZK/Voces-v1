508663467bf22c2daed0c8f4a200c26694c9869f
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-19 17:16:39 -0700

    add featured voices component

---
+++ Voces/client/src/Components/FeaturedVoices.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../CSS/Featured.css';
+
+export default function FeatureVoices() {
+  return (
+    <div className="border">
+      <h1>Featured News</h1>
+    </div>
+  )
+}
