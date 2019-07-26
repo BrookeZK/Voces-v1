3077b23e01f276dc2e3bea0244fc1c36ebfeee70
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-26 15:25:02 -0700

    add text and styling

--- Voces/client/src/Components/Home/FeaturedVideo.tsx
+++ Voces/client/src/Components/Home/FeaturedVideo.tsx
@@ -3,8 +3,14 @@ import '../../CSS/Featured.css';
 
 export default function FeatureVideo() {
   return (
-    <div className="border video">
-      <h1>Featured Video</h1>
+    <div className="video">
+      <div className="vid-feature-content">
+        <h1>Tomado en el borde</h1>
+        <em>Un vidéo sobre el estado de la inmigración en los EEUU</em>
+        <p>Los migrantes en México enfrentan secuestros y violencia mientras esperan audiencias de inmigración en los EE. UU</p>
+        <strong>>> Ver Más >></strong>
+      </div>
+      {/* <h1>Featured Video</h1> */}
     </div>
   )
 }
