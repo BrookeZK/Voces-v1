b77577a195ec321be32315fefd59023c89298308
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-26 10:03:34 -0700

    update styling on Links to remove text decoration

--- Voces/client/src/Components/Nav.tsx
+++ Voces/client/src/Components/Nav.tsx
@@ -7,12 +7,12 @@ export default function Nav() {
     <div>
       <div className="myNav">
         <ul>
-          <li><Link to="/editoriales">Editoriales</Link></li>
-          <li><Link to="/vidéos">Vidéos</Link></li>
-          <li><Link to="/voces-de-la-communidad">Voces de la Communidad</Link></li>
-          <li><Link to="/notícias">Notícias</Link></li>
-          <li><Link to="/cartas-a-la-editora">Cartas a la Editora</Link></li>
-          <li><Link to="/quiénes-sómos">¿Quiénes Sómos?</Link></li>
+          <li><Link className="link" to="/editoriales">Editoriales</Link></li>
+          <li><Link className="link" to="/vidéos">Vidéos</Link></li>
+          <li><Link className="link" to="/voces-de-la-communidad">Voces de la Communidad</Link></li>
+          <li><Link className="link" to="/notícias">Notícias</Link></li>
+          <li><Link className="link" to="/cartas-a-la-editora">Cartas a la Editora</Link></li>
+          <li><Link className="link" to="/quiénes-sómos">¿Quiénes Sómos?</Link></li>
         </ul>
       </div>
     </div>
