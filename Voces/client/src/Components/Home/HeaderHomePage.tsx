413c47018c980ef43b69dd63a8e952ea90203cce
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-26 14:02:13 -0700

    add link to Voces header

--- Voces/client/src/Components/Home/HeaderHomePage.tsx
+++ Voces/client/src/Components/Home/HeaderHomePage.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../../CSS/NavHeader.css';
+import { Link } from 'react-router-dom';
 
 export default function HeaderHomePage() {
   return (
     <div className="header">
-        <p><span className="title1">V</span><span className="title2">oces</span></p>
+        <p><Link to='/' className="link"><span className="title1">V</span><span className="title2">oces</span></Link></p>
     </div>
   )
 }
