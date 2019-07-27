8f3825cfef6e86ce6e78b827194642f2a90167eb
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-26 23:12:07 -0700

    add embeded video to video page

--- Voces/client/src/Components/Video/Video.tsx
+++ Voces/client/src/Components/Video/Video.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import '../../CSS/VideoPage.css';
+import ReactPlayer from 'react-player';
 
 export default function Video() {
   return (
     <div>
-      <h1>vid area</h1>
+      <div>
+        <ul className="videoNav">
+          <li>Inmigración</li>
+          <li>Protesta</li>
+          <li>Celebracion</li>
+          <li>Resistencia</li>
+          <li>Arte</li>
+        </ul>
+      </div>
+      <div className="video">
+        <div className='player-wrapper'>
+          <ReactPlayer
+            className='react-player'
+            url='https://vimeo.com/300390166'
+            width='100%'
+            height='93vh'
+          />
+        </div>
+      </div>
     </div>
   )
 }
