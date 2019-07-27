2ce951fe72a971b7cc39175e0c118e4c9b0cc761
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-26 21:53:48 -0700

    add message and video area to video page

--- Voces/client/src/Components/Video/VideosPage.tsx
+++ Voces/client/src/Components/Video/VideosPage.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import NavGeneral from '../NavGeneral';
+import '../../CSS/VideoPage.css';
+import Video from './Video';
 
 export default function VideosPage() {
   return (
     <div>
+      <NavGeneral />
+      <div className="strongMessage">
+        <div>
+          <h1 className="stronMessageTitle">Se le deba la voz a la communidad.</h1>
+          <h2>Únete a nosotros en la mission de dar voz a la communidad pan-hispana estadounidense. <em>Voces</em> cubre a fondo los asuntos más importantes y celebratorios de la communidad. Descruba las historias de la gente en vidéo. Comparta. Discuta. Aprenda.</h2>
+        </div>
+      </div>
+      <div className="videoViewer">
+        <Video />
+      </div>
     </div>
   )
 }
