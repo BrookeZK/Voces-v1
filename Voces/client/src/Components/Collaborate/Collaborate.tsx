92ba1ce933cc476e86a55144e12d9547c19897b4
Student <example@example.com>
2019-07-29 13:57:06 -0700

    add collaborate component

---
+++ Voces/client/src/Components/Collaborate/Collaborate.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../CSS/Collaborate.css';
+
+export default function Collaborate() {
+  return (
+    <div className="collaborate">
+        <h1>¡Colabore con nosotros!</h1>
+        <h4>Busquemos crecer nuestra communidad de jornalistas. Si sea jornalista de largo tiempo, apasionadx para apollar la vox de la communidad; si sea joven un tu carera y quieres publicar tus primeros articulos; si sea miembro de la communidad con opinión a compartir; o si sepa de una historia que merece cubrimiento: <strong>¡Contáctenos!</strong></h4>
+    </div>
+  )
+}
