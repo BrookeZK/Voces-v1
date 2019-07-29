794e8fb37562c8e661233ccbba3c6d595ed78103
Student <example@example.com>
2019-07-29 14:49:49 -0700

    update route path for editorial component

--- Voces/client/src/Components/Editorial/EditorialPage.tsx
+++ Voces/client/src/Components/Editorial/EditorialPage.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import NavGeneral from '../NavGeneral';
+import Footer from '../Footer';
 
 export default function EditorialPage() {
   return (
-    <div>
+    <div className="main-content">
+    <div id="content-wrapper">
+      <NavGeneral />
+      <div className="">
+        
+      </div>
+      <div >
+        
+      </div>
+      <div id="footer">
+        <Footer />
+      </div>
     </div>
+  </div>  
   )
 }
