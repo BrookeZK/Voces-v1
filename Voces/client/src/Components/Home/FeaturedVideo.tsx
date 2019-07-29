b956a21caba9fb26fc7efed35bfe1ceea52a8c60
Student <example@example.com>
2019-07-29 14:45:01 -0700

    add styling for link

--- Voces/client/src/Components/Home/FeaturedVideo.tsx
+++ Voces/client/src/Components/Home/FeaturedVideo.tsx
@@ -9,7 +9,7 @@ export default function FeatureVideo() {
         <h1>Tomado en el borde</h1>
         <em>Un vidéo sobre el estado de la inmigración en los EEUU</em>
         <p>Los migrantes en México enfrentan secuestros y violencia mientras esperan audiencias de inmigración en los EE. UU</p>
-        <Link to='/videos#vid'><strong>>> Ver Más >></strong></Link>
+        <Link className="link-white" to='/videos#vid'><strong>>> Ver Más >></strong></Link>
       </div>
       {/* <h1>Featured Video</h1> */}
     </div>
