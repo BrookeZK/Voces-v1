fd7fc3b540e51942b6b44e83ca85ac13b1d5f840
Brooke Kullberg <brookekullberg@gmail.com>
2019-08-01 00:41:51 -0700

    add png logo to the home page header

--- Voces/client/src/Components/Home/HeaderHomePage.tsx
+++ Voces/client/src/Components/Home/HeaderHomePage.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../../CSS/NavHeader.css';
 import { Link } from 'react-router-dom';
+import logoLarge from '../../assets/voces-logo-large.png';
 
 export default function HeaderHomePage() {
   return (
     <div className="header">
-        <p><Link to='/' className="link"><span className="title1">V</span><span className="title2">oces</span></Link></p>
+      <p><Link to='/'><img src={logoLarge} alt="voces logo"/></Link></p>
     </div>
   )
 }
