62a070f62828181c8e99f377a7a88ba8779afbcd
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-31 23:05:33 -0700

    add progress on css grid

--- Voces/client/src/Components/Home/FeaturedNews.tsx
+++ Voces/client/src/Components/Home/FeaturedNews.tsx
@@ -3,8 +3,29 @@ import '../../CSS/Featured.css';
 
 export default function FeatureNews() {
   return (
-    <div className="border news">
-      <h1>Featured News</h1>
+    <div className="newsFeatured">
+      <h1>En Las Noticias</h1>
+      <div className="newsGrid">
+        <div className="news1">1</div>
+        <div className="news2">2</div>
+        <div className="news3">3</div>
+        <div className="news4">4</div>
+        <div className="news5">5</div>
+        <div className="news6">6</div>
+        <div className="news7">7</div>
+        <div className="news8">8</div>
+        <div className="news9">9</div>
+        <div className="news10">10</div>
+      </div>
     </div>
   )
 }
+
+// e r
+// d r
+// d f
+// g m
+// h m
+// h z
+// x z
+// x c
