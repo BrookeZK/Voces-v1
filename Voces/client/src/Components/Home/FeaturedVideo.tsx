44f7f6e58b208d95525dd9e7a2d480f02e090967
Brooke Kullberg <brookekullberg@gmail.com>
2019-08-01 01:21:19 -0700

    remove unecessary code

--- Voces/client/src/Components/Home/FeaturedVideo.tsx
+++ Voces/client/src/Components/Home/FeaturedVideo.tsx
@@ -9,9 +9,8 @@ export default function FeatureVideo() {
         <h1>Tomado en la frontera</h1>
         <em>Un vidéo sobre el estado de la inmigración en los EEUU</em>
         <p>Los migrantes en México enfrentan secuestros y violencia mientras esperan audiencias de inmigración de los Estados Unidos</p>
-        <Link className="link-white" to='/videos#vid'><strong>>> Ver Más >></strong></Link>
+        <Link className="link-white" to='/videos#vid'><strong >>> Ver Más >></strong></Link>
       </div>
-      {/* <h1>Featured Video</h1> */}
     </div>
   )
 }
