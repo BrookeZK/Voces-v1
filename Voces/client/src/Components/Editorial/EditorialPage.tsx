675c54f63f1c1450c96082c01ed4c08865c50e60
Student <example@example.com>
2019-08-01 10:04:17 -0700

    change italicize to subheader

--- Voces/client/src/Components/Editorial/EditorialPage.tsx
+++ Voces/client/src/Components/Editorial/EditorialPage.tsx
@@ -11,8 +11,8 @@ export default function EditorialPage() {
         <NavGeneral />
         <div className="center-editorial">
           <div className="editorial-content">
-            <h1 className="editorial-title edtorial-padding"><em>Trauma en los niños que no saben si sus padres van a regresar</em></h1>
-            <h2 className="editorial-subtitle edtorial-padding">Una entrevista con <strong>Kat Kelley</strong>, directora de Iniciativas Estratégicas de Caridades Católicas de Oregon.</h2>
+            <h1 className="editorial-title edtorial-padding">Trauma en los niños que no saben si sus padres van a regresar</h1>
+            <h2 className="editorial-subtitle edtorial-padding"><em>Una entrevista con <strong>Kat Kelley</strong>, directora de Iniciativas Estratégicas de Caridades Católicas de Oregon.</em></h2>
             <img id="hero-image" src={children} alt="children" />
             <p className="img-text">Niños en la escuela. ¿Cómo es no saber adónde están tus padres, ni adónde vas? <span className="img-src">Angie Angeles/El Centinela</span></p>
             <h4 className="editorial-author">Por Rocio Rios</h4>
