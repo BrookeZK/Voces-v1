786bbbafbbede7d0d9a09d807bd44bd716bf3f7d
Brooke Kullberg <brookekullberg@gmail.com>
2019-08-01 00:58:11 -0700

    update styling for consistency

--- Voces/client/src/Components/Home/FeaturedVoices.tsx
+++ Voces/client/src/Components/Home/FeaturedVoices.tsx
@@ -3,7 +3,7 @@ import '../../CSS/Featured.css';
 
 export default function FeaturedVoices() {
   const pink = {
-    color: 'rgb(207, 142, 208)'
+    color: 'rgb(196, 79, 198)'
   }
 
   // rgb(243, 171, 244)
@@ -27,27 +27,22 @@ export default function FeaturedVoices() {
     backgroundColor: 'rgb(0, 0, 0, 0.5)'
   }
 
-  const background3 = {
-    backgroundColor: 'rgb(0, 0, 0, 0.5)',
-    color: 'rgb(207, 142, 208)'
-  }
-
   return (
     <div className="voicesFeatured">
       <h1><em><strong>| | | Tú Voz</strong></em></h1>
       <div className="feature-voices-flex">
         <div className="featureVoice1">
-          <h2><span className="name-highlight" style={pink}>Natalia</span> conoce su gente.</h2>
+          <h2><span className="name-highlight" style={pink}>Natalia</span> conoce a su gente.</h2>
           <div className="featuredTagLine">
             <h3 style={background2}>"El sueño vive". Su historia creando un espacio communal de arte latinx.</h3>
-            <h3 style={pink}>>> Mira el vidéo >></h3>
+            <h3 style={pink} className="seeMore">>> Mira >></h3>
           </div>
         </div>
         <div className="featureVoice2">
-          <h2 className="name-highlight2" style={green}>Latino<br></br> Magico</h2>
+          <h2 className="name-highlight2" style={green}>Latino Mágico</h2>
           <div className="featuredTagLine2">
             <h3 style={background1}>Martin Allais discute sus raices multiculturales, la fuerza de una identidad fronteriza, y su arte.</h3>
-            <h3 style={green}>>> Mira el vidéo >></h3>
+            <h3 style={green} className="seeMore">>> Mira >></h3>
           </div>
         </div>
       </div>
