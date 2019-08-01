9d011d178e23fbd61ec59334fe497c10f4da50f3
Student <example@example.com>
2019-07-31 17:37:03 -0700

    fix content typo

--- Voces/client/src/Components/Home/FeaturedVideo.tsx
+++ Voces/client/src/Components/Home/FeaturedVideo.tsx
@@ -6,9 +6,9 @@ export default function FeatureVideo() {
   return (
     <div className="videoFeatured">
       <div className="vid-feature-content">
-        <h1>Tomado en el borde</h1>
+        <h1>Tomado en la frontera</h1>
         <em>Un vidéo sobre el estado de la inmigración en los EEUU</em>
-        <p>Los migrantes en México enfrentan secuestros y violencia mientras esperan audiencias de inmigración en los EE. UU</p>
+        <p>Los migrantes en México enfrentan secuestros y violencia mientras esperan audiencias de inmigración de los Estados Unidos</p>
         <Link className="link-white" to='/videos#vid'><strong>>> Ver Más >></strong></Link>
       </div>
       {/* <h1>Featured Video</h1> */}
