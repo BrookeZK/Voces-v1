94a08034ad85d46d0edf1d6dde5284b9cbef8b49
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-31 20:11:04 -0700

    add basic content to communityvoicespage

--- Voces/client/src/Components/CommunityVoices/CommunityVoicesPage.tsx
+++ Voces/client/src/Components/CommunityVoices/CommunityVoicesPage.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import NavGeneral from '../NavGeneral';
+import Footer from '../Footer'
+import Collaborate from '../Collaborate/Collaborate';
+import VideoCommunity from './VideoCommunity';
 
 export default function CommunityVoicesPage() {
   return (
-    <div>
-    </div>
+
+    <div className="main-content">
+      <div id="content-wrapper">
+        <NavGeneral />
+        <div className="voices">
+          <div>
+           
+          </div>
+        </div>
+        <div id="vid" className="videoViewer">
+          <VideoCommunity />
+        </div>
+        <Collaborate />
+        <div id="footer">
+          <Footer />
+        </div>
+      </div>
+    </div>  
   )
 }
