128d95fed80b255659beb73ac877c8ef345d0967
Student <example@example.com>
2019-08-01 09:30:32 -0700

    change fonts across site

--- Voces/client/src/Components/Home/FeaturedEditorial.tsx
+++ Voces/client/src/Components/Home/FeaturedEditorial.tsx
@@ -3,12 +3,17 @@ import '../../CSS/Featured.css';
 import { HashLink as Link } from 'react-router-hash-link';
 
 export default function FeatureEditorial() {
+
+  const style = {
+    fontSize: "25px"
+  }
+
   return (
     <div className="editorialFeatured">
       <div className="editorial-feature-content">
         <h1>Trauma en los niños que no saben si sus padres van a regresar</h1>
-        <em>Una entrevista con <strong>Kat Kelley</strong>, directora de Iniciativas Estratégicas de Caridades Católicas de Oregon.</em><br></br><br></br>
-        <p className="hoverEffect"><Link className="link" to='/editorial#nav'><strong>>> Leer Más >></strong></Link></p>
+        <em style={style}>Una entrevista con <strong>Kat Kelley</strong>, directora de Iniciativas Estratégicas de Caridades Católicas de Oregon.</em><br></br><br></br>
+        <p style={style} className="hoverEffect"><Link className="link" to='/editorial#nav'><strong>>> Leer Más >></strong></Link></p>
       </div>  
     </div>
   )
