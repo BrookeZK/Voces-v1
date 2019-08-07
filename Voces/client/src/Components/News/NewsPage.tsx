79569b60db28221e6209b3a242781921daa3947c
Brooke Kullberg <brookekullberg@gmail.com>
2019-08-06 23:08:14 -0700

    Updates

--- Voces/client/src/Components/News/NewsPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function NewsPage() {
-  return (
-    <div>
-    </div>
-  )
-}
---
+++ precache-manifest.cbf8f6e720114e62b08406489dfdc948.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8006e89bd266f07672b0997f612e3620",
+    "url": "/Voces/index.html"
+  },
+  {
+    "revision": "666343a5daba6ba2a413",
+    "url": "/Voces/static/css/main.9e8432ee.chunk.css"
+  },
+  {
+    "revision": "1b6d4f112567b5d29478",
+    "url": "/Voces/static/js/2.f0a5be6d.chunk.js"
+  },
+  {
+    "revision": "666343a5daba6ba2a413",
+    "url": "/Voces/static/js/main.af5d88a7.chunk.js"
+  },
+  {
+    "revision": "58aa5b3bfe789a01cae5",
+    "url": "/Voces/static/js/runtime~main.39a0a7ac.js"
+  },
+  {
+    "revision": "bd7887c7fdb04edd01908ce2d6a2eb9e",
+    "url": "/Voces/static/media/children.bd7887c7.jpg"
+  },
+  {
+    "revision": "8014ac041f9c885a33e8b6ea505206b5",
+    "url": "/Voces/static/media/feature1.8014ac04.png"
+  },
+  {
+    "revision": "2c3f9c60758bcb2721c5b54c89e38797",
+    "url": "/Voces/static/media/feature2.2c3f9c60.png"
+  },
+  {
+    "revision": "6a6e5ff59220cb7710f37d8b6e15ebcf",
+    "url": "/Voces/static/media/girl-taken-at-border.6a6e5ff5.png"
+  },
+  {
+    "revision": "cf3d3dd6eaedbd835a84bd90526376ab",
+    "url": "/Voces/static/media/immigration-vid-text.cf3d3dd6.png"
+  },
+  {
+    "revision": "49d6200d9655f47256cd3e59fe198276",
+    "url": "/Voces/static/media/student-health.49d6200d.jpg"
+  },
+  {
+    "revision": "0d4438915d9e2218cf3f94925939d4a7",
+    "url": "/Voces/static/media/voces-logo-large.0d443891.png"
+  }
+]);
